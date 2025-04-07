f3ff128438b1555bb16133e5062cf76e00d20be7
mike seibel <mikes@washington.edu>
2025-04-07 13:48:23 -0700

    initial lti 1.3 config

--- grading_standard/urls.py
+++ grading_standard/urls.py
@@ -6,7 +6,7 @@ from django.views.i18n import JavaScriptCatalog
 from grading_standard.views import LaunchView, GradingStandardView
 
 urlpatterns = [
-    re_path(r'^$', LaunchView.as_view()),
+    re_path(r'^$', LaunchView.as_view(), name="lti-launch"),
     re_path(r'^api/v1/grading_standards/$', GradingStandardView.as_view()),
     re_path(r'^api/v1/grading_standards/(?P<grading_standard_id>[^/]*)$',
             GradingStandardView.as_view()),
