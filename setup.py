89a1e8bf159ca7048124d1a9f67d9415db1d8de8
Jim Laney <jlaney@uw.edu>
2025-12-19 08:23:31 -0800

    updates blti library

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ See the README on `GitHub
 <https://github.com/uw-it-aca/grading-standard-lti>`_.
 """
 
-version_path = 'grading_standard/VERSION'
+version_path = "grading_standard/VERSION"
 VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
 VERSION = VERSION.replace("\n", "")
 
@@ -14,32 +14,32 @@ VERSION = VERSION.replace("\n", "")
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='UW-Grading-Standard-LTI',
+    name="UW-Grading-Standard-LTI",
     version=VERSION,
-    packages=['grading_standard'],
+    packages=["grading_standard"],
     include_package_data=True,
     install_requires = [
-        'django~=5.2',
-        'django-blti~=3.0',
-        'django-compressor',
-        'uw-memcached-clients~=1.0',
-        'uw-restclients-core~=1.4',
-        'uw-restclients-canvas~=1.2',
+        "django~=5.2",
+        "django-blti~=3.0",
+        "django-compressor",
+        "uw-memcached-clients~=1.0",
+        "uw-restclients-core~=1.4",
+        "uw-restclients-canvas~=1.2",
     ],
-    license='Apache License, Version 2.0',
+    license="Apache License, Version 2.0",
     description=(
-        'An LTI app for creating 4.0 grading standards and adding them to a '
-        'Canvas course'),
+        "An LTI app for creating 4.0 grading standards and adding them to a "
+        "Canvas course"),
     long_description=README,
-    url='https://github.com/uw-it-aca/grading-standard-lti',
+    url="https://github.com/uw-it-aca/grading-standard-lti",
     author="UW-IT Student & Educational Technology Services",
     author_email="aca-it@uw.edu",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
 )
